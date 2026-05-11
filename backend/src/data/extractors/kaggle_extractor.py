bc79818fa25f1519ee431f9cb0be3217d796ce96
Archel Taneka <archeltaneka@gmail.com>
2026-05-11 10:43:43 +1000

    [Fix] Dynamically detect whether the downloaded file is a ZIP archive

--- backend/src/data/extractors/kaggle_extractor.py
+++ backend/src/data/extractors/kaggle_extractor.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import kagglehub
 import os
+import zipfile
 
 
 def fetch_json_from_kaggle(dataset: str, filename: str, output_dir: str):
     """Fetches data from Kaggle and returns a pandas DataFrame."""
-    kagglehub.dataset_download(dataset, path=filename, output_dir=output_dir)
-    df = pd.read_json(os.path.join(output_dir, filename), compression="zip")
-    return df
+    path = kagglehub.dataset_download(dataset, path=filename, output_dir=output_dir)
+    compression = "zip" if zipfile.is_zipfile(path) else None
+    return pd.read_json(path, compression=compression)
 
 
 def fetch_csv_from_kaggle(dataset: str, filename: str, output_dir: str, separator: str, usecols: list[str]):
     """Fetches data from Kaggle and returns a pandas DataFrame."""
-    kagglehub.dataset_download(dataset, path=filename, output_dir=output_dir)
-    return pd.read_csv(os.path.join(output_dir, filename), sep=separator, usecols=usecols, compression="zip")
+    path = kagglehub.dataset_download(dataset, path=filename, output_dir=output_dir)
+    compression = "zip" if zipfile.is_zipfile(path) else None
+    return pd.read_csv(path, sep=separator, usecols=usecols, compression=compression)
